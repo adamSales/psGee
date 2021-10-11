c0963276a7065b9cb8b7fb03a603ab1d71c5353e
adamSales <asales@wpi.edu>
2021-10-11 16:51:01 -0500

    changes to threeStep.r

--- code/threeStep.r
+++ code/threeStep.r
@@ -21,38 +21,22 @@ pointEst <- function(Y0,S1,X1,X0,...){
     ## Yp=mu1*p^2+mu0*p*(1-p)+beta'X*p
     ## XY=mu1*p*X+mu0*(1-p)*X+beta*X*X
 
-    a <- rbind(
-        c(mu1=sum(ps), mu0=sum(1-ps), colSums(X0)),
-        c(mu1=sum(ps^2), mu0=sum(ps*(1-ps)), t(ps)%*%X0),
-        cbind(t(X0)%*%ps,t(X0)%*%(1-ps),crossprod(X0))
-    )
-
-    b <- c(sum(Y0),sum(ps*Y0),t(Y0)%*%X0)
-
-    solve(a,b)
-}
-
-bdiag <- function(m1,m2)
-    cbind(
-        rbind(m1,matrix(0,nrow(m1),ncol(m2))),
-        rbind(matrix(0,nrow(m2),ncol(m1)),m2)
+    n <- length(ps)
+    A <- lapply(1:n,function(i)
+        rbind(
+            c(mu1=ps[i],mu0=1-ps[i],X0[i,]),
+            c(ps[i]^2,ps[i]*(1-ps[i]),ps[i]*X0[i,]),
+            cbind(X0[i,]*ps[i],X0[i,]*(1-ps[i]),X0[i,]%*%t(X0[i,]))
+        )
         )
 
+    B <- lapply(1:n, function(i) c(Y0[i],ps[i]*Y0[i],Y0[i]*X0[i,]))
 
-A11 <- function(logit1,ols1)
-    bdiag(bread(logit1),bread(ols1))
+    est <- solve(Reduce("+",A),Reduce("+",B))
 
-meat12 <- function(logit1,ols1,adjust=TRUE){
-
-    psi <- cbind(estfun(logit1),estfun(ols1))
-    k <- NCOL(psi)
-    n <- NROW(psi)
-    rval <- crossprod(as.matrix(psi))/n
-    if (adjust)
-        rval <- n/(n - k) * rval
-    rownames(rval) <- colnames(rval) <- colnames(psi)
-    rval
+    list(est=est,ef=do.call('rbind',lapply(1:n,function(i) t(B[[i]]-A[[i]]%*%est))))
 }
+
 
 A21 <- function(logit1,Y0,mu01,mu00,beta0,X0){
 ### derivative of step 3 EEs as functions of logit coefs
@@ -65,8 +49,7 @@ A21 <- function(logit1,Y0,mu01,mu00,beta0,X0){
         t(pq*(Y0-X0%*%beta0-mu00))%*%Xtilde-2*(mu01-mu00)*t(p*pq)%*%Xtilde,
         -(mu01-mu00)*t(sweep(X0,1,pq,"*"))%*%Xtilde
     )
-    ## append 0s for first OLS regression
-    cbind(mat,matrix(0,nrow(mat),ncol(X)+2))
+    mat
 }
 
 A22 <- function(logit1,X0){
@@ -89,20 +72,23 @@ ef2 <- function(Y0,X0,mu1,mu0,logit1,beta0){
     )
 }
 
-B12 <- function(logit1,ols1,Y0,X0,mu1,mu0,beta0)
-    t(cbind(estfun(logit1),estfun(ols1)))%*%ef2(Y0,X0,mu1,mu0,logit1,beta0)
+B12 <- function(logit1,Y0,X0,mu1,mu0,beta0){
+    EF1 <- estfun(logit1)
+    EF0 <- ef2(Y0,X0,mu1,mu0,logit1,beta0)
+    t(rbind(EF1,matrix()%*%
 
 B22 <- function(Y0,X0,mu1,mu0,logit1,beta0)
     crossprod(ef2(Y0,X0,mu1,mu0,logit1,beta0))
 
-vcv2 <- function(dat,est,logit1,ols1){
-    a11inv <- A11(logit1,ols1)
-    a21 <- A21(logit1,dat$Y0,est$mu01,est$mu00,est$beta0,dat$X0)
+vcv2 <- function(dat,est,ef,logit1){
+    a11inv <- bread(logit1)
+    a21 <- A21(logit1=logit1,Y0=dat$Y0,est$mu1,est$mu0,est$beta0,dat$X0)
     a22 <- A22(logit1,dat$X0)
-    b11 <- cbind(meat(logit1),meat(ols1))
-    b12 <- B12(logit1,ols1,dat$Y0,dat$X0,est$mu1,est$mu0,est$beta0)
+    b11 <- meat(logit1)
+    b12 <- B12(logit1,dat$Y0,dat$X0,est$mu1,est$mu0,est$beta0)
     b22 <- B22(dat$Y0,dat$X0,est$mu1,est$mu0,logit1,est$beta0)
 
-    n <- length(dat$Y)
-    solve(a22)%*%(b22-a21%*%a11inv%*%b12-t(b12)%*%t(a11inv)%*%t(a21)+a21%*%a11inv%*%b11%*%t(a11inv)%*%t(a21))/n
+    n <- length(dat$Y0)
+    solve(a22)%*%(b22+a21%*%a11inv%*%b11%*%t(a11inv)%*%t(a21))/n
 
+}
