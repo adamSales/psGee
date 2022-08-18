82b5bf130c55481c72f6cdd9514242369d3745c4
adamSales <asales@wpi.edu>
2022-08-18 14:24:44 -0500

    kirk's work; update data.r

--- code/fh2t/data.r
+++ code/fh2t/data.r
@@ -5,34 +5,18 @@ select <- dplyr::select
 
 Scale <- function(x) (x-mean(x,na.rm=TRUE))/sd(x,na.rm=TRUE)
 
-actDat0=read_csv('data/logs_for_adam.csv')
-
 dat=read_csv('data/DATA20220202_3591.csv',na=c('','NA','#NULL!'))
 
-probMeta=read_csv('data/ASSISTments-Table 1.csv')
-probMeta=select(probMeta,!starts_with('...'))
-names(probMeta)=gsub(' ','',names(probMeta))
-probMeta$Hints[probMeta$Note=='3']=3
+kirk <- read_csv('data/data_uptated_8_18.csv')
 
-bottomOut=
-  probMeta%>%
-  filter(Hints!='n/a',CorrectAnswer!='n/a')%>%
-  mutate(Hints=as.numeric(Hints),
-         Subproblem=ifelse(Subproblem%in%c('n/a','N/A'),'A',Subproblem))%>%
-  select(ProblemID,ProblemSet,ProblemNumber,ProblemOrder,Hints,Subproblem)%>%
-  inner_join(mutate(actDat0,Subproblem=LETTERS[problem_part]),
-             by=c("ProblemID"="problem_id","Subproblem"))%>%
-  select(-'...1')%>%
-  group_by(StuID)%>%
-  summarize(nbo=sum(action=='answer_hint',na.rm=TRUE),nprob=n_distinct(ProblemID))
+dat$nbo <- kirk$num_problem_parts_used_bottom_out_hint[match(dat$StuID,kirk$StuID)]
+dat <- filter(dat,!is.na(post.total_math_score))
 
-dat <- left_join(dat,bottomOut)%>%
-  filter(!is.na(post.total_math_score))
 
 cat(sum(is.na(dat$nbo)&dat$rdm_condition=='ASSISTments'),' students with NA for # bottom out hints; setting to 0\n')
-
 dat$nbo[is.na(dat$nbo)&dat$rdm_condition=='ASSISTments'] <- 0
 
+plot(table(dat$nbo[dat$rdm_condition=='ASSISTments']),xlim=c(0,30))
 med <- median(dat$nbo[dat$rdm_condition=='ASSISTments'],na.rm=TRUE)
 
 dat$S <- dat$nbo>med
@@ -45,12 +29,67 @@ psdat <- dat%>%
       fct_lump_min(200)%>%
       fct_recode(`Hispanic/Latino`="1",Asian="3",White="6")%>%
       fct_relevel('White'),
-    Gender=as.factor(Gender))
+    Gender=as.factor(Gender))%>%
+  rename(trt=rdm_condition)
 
-imp <- psdat%>%
-  select(virtual, Gender,raceEth,Performance.Level5,EIP,Scale.Score5:PercentInAttendance6,starts_with("pre"))%>%
-  missForest()
+impDat <- psdat%>%
+  select(virtual, Gender,raceEth,Performance.Level5,EIP,Scale.Score5:PercentInAttendance6,starts_with("pre",ignore.case=FALSE))%>%
+  select(where(~mean(is.na(.))<0.2))%>%
+  select(where(~n_distinct(.,na.rm=TRUE)>1))%>%
+  map_dfc(~if(is.character(.)|n_distinct(.,na.rm=TRUE)<4) as.factor(.) else .)%>%
+  as.data.frame()
 
-names(imp$ximp) <- paste0(names(imp$xmiss)
+Rho=cor(impDat[,sapply(impDat,class)=='numeric'],method='spearman',use='pairwise')
+Rho[upper.tri(Rho,diag=TRUE)] <- NA
+bigCor=which(!is.na(Rho)&abs(Rho)>0.8,arr.ind=TRUE)
+sapply(1:nrow(bigCor),function(i) c(rownames(Rho)[bigCor[i,1]],colnames(Rho)[bigCor[i,2]],round(Rho[bigCor[i,1],bigCor[i,2]],2)))%>%t()
 
-save(dat,file='psdat.RData')
+impDat <- impDat[,!names(impDat)%in%rownames(bigCor)]
+Rho=cor(impDat[,sapply(impDat,class)=='numeric'],method='spearman',use='pairwise')
+Rho[upper.tri(Rho,diag=TRUE)] <- NA
+round(tail(sort(abs(Rho))),3)
+
+### now for factors
+cramerV = function(x, y){
+  N      = length(x)
+  Chi.sq = suppressWarnings(chisq.test(x, y, correct=FALSE)$statistic)
+  Phi    = Chi.sq / N
+  Row    = length(unique(x))
+  C      = length(unique(y))
+  sqrt(Phi / min(Row-1, C-1))
+}
+
+fac=which(sapply(impDat,class)=='factor')
+V <- matrix(nrow=length(fac)-1,ncol=length(fac)-1,dimnames=list(names(fac)[-1],names(fac)[-length(names(fac))]))
+for(i in 2:(length(fac))) for(j in 1:(i-1)) V[i-1,j] <- cramerV(impDat[,fac[i]],impDat[,fac[j]])
+### keep all factors
+
+imp <- missForest(impDat,variablewise = TRUE)
+
+save(imp,file='data/imputations.RData')
+
+mse=which(names(imp$OOBerror)=='MSE')
+names(imp$OOBerror) <- names(impDat)
+
+r2imp=1-imp$OOBerror[mse]/sapply(impDat[,mse],var,na.rm=TRUE)
+
+#names(imp$ximp) <- paste0(names(imp$ximp),'Imp')
+
+naFlags <- map_dfc(impDat,is.na)%>%select(where(~sum(.)>1))%>%as.data.frame()
+V <- matrix(nrow=ncol(naFlags)-1,ncol=ncol(naFlags)-1,dimnames=list(names(naFlags)[-1],names(naFlags)[-ncol(naFlags)]))
+for(i in 2:(ncol(naFlags))) for(j in 1:(i-1)) V[i-1,j] <- cramerV(naFlags[,i],naFlags[,j])
+
+naFlags <- naFlags[,!names(naFlags)%in%rownames(which(V>0.8,arr.ind=TRUE))]
+names(naFlags) <- paste0(names(naFlags),'NA')
+
+psdat <- cbind(psdat[,c('StuID','SchIDPre','TeaIDPre_within_school','ClaIDPre','trt','S','Y')],
+               imp$ximp,
+               naFlags)
+
+psdat <- psdat%>%
+  rename(TeaIDPre=TeaIDPre_within_school)%>%
+  mutate(across(ends_with('IDPre'),as.factor))
+
+psdat$TeaIDPre <- relevel(psdat$TeaIDPre,ref='78') ## biggest in ASSISTments group
+
+save(psdat,file='data/psdat.RData')
